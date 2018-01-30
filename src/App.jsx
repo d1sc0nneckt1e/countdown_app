a5455511eaa89a11aa922dc9e00aac951719055f
d1sc0nneckt1e <a.c.rezai@gmail.com>
2018-01-30 23:43:42 +0100

    structuring the app / adding css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
+import './app.css';
 
 class App extends Component{
   render(){
     return(
-      <div>Countdown App Component</div>
+      <div className='App'>
+        <div className='App-title'>Countdown to December 25, 2018</div>
+        <div className='clock'>
+          <div className='clock-timer'>14 Days</div>
+          <div className='clock-timer'>30 Hours</div>
+          <div className='clock-timer'>15 Minutes</div>
+          <div className='clock-timer'>20 Seconds</div>
+        </div>
+          <div>
+            <input placeholder='New Date'/>
+            <button>Submit</button>
+          </div>
+      </div>
     )
   }
 }
