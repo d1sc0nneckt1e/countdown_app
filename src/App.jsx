ac1187bb9e024a7fa536e3e2b281ac04dbbcbf01
d1sc0nneckt1e <a.c.rezai@gmail.com>
2018-01-31 13:40:51 +0100

    created clock component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Clock from './clock';
 import './app.css';
 
 class App extends Component{
@@ -19,12 +20,7 @@ class App extends Component{
     return(
       <div className='App'>
         <div className='App-title'>Countdown to {this.state.deadline}</div>
-        <div className='clock'>
-          <div className='clock-timer'>14 Days</div>
-          <div className='clock-timer'>30 Hours</div>
-          <div className='clock-timer'>15 Minutes</div>
-          <div className='clock-timer'>20 Seconds</div>
-        </div>
+        <Clock/>
           <div>
             <input placeholder='New Date' onChange={event => this.setState({newDeadline: event.target.value})}/>
             <button onClick={() => this.changeDeadline()}>Submit</button>
