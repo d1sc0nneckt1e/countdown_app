b96b33a1d5832a8ec0682194c93a5f295598ad47
d1sc0nneckt1e <a.c.rezai@gmail.com>
2018-01-31 13:13:51 +0100

    using and updating state

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,21 @@ import React, {Component} from 'react';
 import './app.css';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2018'
+    }
+  }
+
+  changeDeadline(){
+    this.setState({deadline: 'November 25. 2018'})
+  }
+
   render(){
     return(
       <div className='App'>
-        <div className='App-title'>Countdown to December 25, 2018</div>
+        <div className='App-title'>Countdown to {this.state.deadline}</div>
         <div className='clock'>
           <div className='clock-timer'>14 Days</div>
           <div className='clock-timer'>30 Hours</div>
@@ -14,7 +25,7 @@ class App extends Component{
         </div>
           <div>
             <input placeholder='New Date'/>
-            <button>Submit</button>
+            <button onClick={() => this.changeDeadline()}>Submit</button>
           </div>
       </div>
     )
